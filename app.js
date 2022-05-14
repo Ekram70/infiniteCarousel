ca80b141c8d24511f9ad05760eae437772815ae0
Ekram70 <ekramullah70@gmail.com>
2022-05-15 02:04:22 +0600

    prev transition not working

---
+++ app.js
@@ -0,0 +1,27 @@
+const carousel = document.querySelector(".carousel");
+const slider = document.querySelector(".slider");
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+let direction = "";
+
+prev.addEventListener("click", function () {
+  slider.style.transform = "translateX(0%)";
+  slider.style.transition = "transform 0.5s ease";
+  slider.prepend(slider.lastElementChild);
+  direction = prev.className;
+});
+
+next.addEventListener("click", function () {
+  slider.style.transform = "translateX(-20%)";
+  slider.style.transition = "transform 0.5s ease";
+  direction = next.className;
+});
+
+slider.addEventListener("transitionend", function () {
+  if (direction == "next") {
+    slider.appendChild(slider.firstElementChild);
+    slider.style.transform = "translateX(0)";
+    slider.style.transition = "";
+  }
+});
